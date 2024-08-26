5b5d5ca7a86c42a382d8495cf5d696fb6a5fb490
Gautam Raj <gautamraj996@gmail.com>
2024-08-26 10:34:40 +0530

    Add lifts

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ let liftSimulationBox = document.querySelector(".lift-simulation");
 function createFloors(floorInputValue) {
   for (let i = floorInputValue; i >= 1; i--) {
     let floorBox = document.createElement("div");
-    floorBox.classList.add("floor-box");
+    floorBox.classList.add(`floor-box`, `floor-${i}`);
 
     // Create button container on each floor
     let buttonContainer = document.createElement("div");
@@ -36,6 +36,19 @@ function createFloors(floorInputValue) {
   }
 }
 
+function createLifts(liftInputValue) {
+  // Create lift container on 1st floor
+  let liftContainer = document.createElement("div");
+  liftContainer.classList.add("lift-container");
+  document.querySelector(".floor-1").appendChild(liftContainer);
+
+  for (let l = 1; l <= liftInputValue; l++) {
+    let liftBox = document.createElement("div");
+    liftBox.classList.add("lift-box");
+    liftContainer.appendChild(liftBox);
+  }
+}
+
 okBtn.addEventListener("click", () => {
   liftSimulationBox.innerHTML = "";
   floorInputValue = floorInput.value;
@@ -43,4 +56,5 @@ okBtn.addEventListener("click", () => {
   console.log(floorInputValue, liftInputValue);
 
   createFloors(Number(floorInputValue));
+  createLifts(Number(liftInputValue));
 });
